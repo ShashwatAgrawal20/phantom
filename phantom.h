89b59f6deca369bc5276a1125bcb40a699db9ae8
Shashwat Agrawal <shashwatagrawal473@gmail.com>
2025-02-25 19:18:50 +0530

    feat(inline formatting): add support for bold, italic, and code

--- phantom.h
+++ phantom.h
@@ -2,12 +2,15 @@
 #define PHANTOM_H
 
 #include <ctype.h>
+#include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-static inline int convert_file(const char *md_path, const char *html_path);
+static int convert_file(const char *md_path, const char *html_path);
+static void print_help(const char *program_name);
+static void _inline_formatting(FILE *html_file, const char *text);
 
 typedef enum {
     OUTSIDE,
@@ -23,7 +26,7 @@ typedef enum {
         goto defer;         \
     } while (0)
 
-static inline void print_help(const char *program_name) {
+static void print_help(const char *program_name) {
     printf("Phantom Markdown to HTML Converter\n");
     printf("\nUsage:\n");
     printf(
@@ -38,6 +41,54 @@ static inline void print_help(const char *program_name) {
     printf("  --help   Show this help message and exit\n");
 }
 
+static void _inline_formatting(FILE *html_file, const char *text) {
+    bool in_bold = false, in_italic = false, in_code = false;
+
+    for (const char *c = text; *c; c++) {
+        if (*c == '`' && !in_bold && !in_italic) {
+            if (!in_code) {
+                fprintf(html_file, "<code>");
+                in_code = true;
+            } else {
+                fprintf(html_file, "</code>");
+                in_code = false;
+            }
+        } else if (*c == '*' && c[1] == '*' && !in_code) {
+            if (!in_bold) {
+                fprintf(html_file, "<strong>");
+                in_bold = true;
+            } else {
+                fprintf(html_file, "</strong>");
+                in_bold = false;
+            }
+            c++;
+        } else if (*c == '*' && c[1] != '*' && !in_code && !in_bold) {
+            if (!in_italic) {
+                fprintf(html_file, "<em>");
+                in_italic = true;
+            } else {
+                fprintf(html_file, "</em>");
+                in_italic = false;
+            }
+        } else {
+            if (*c == '<')
+                fprintf(html_file, "&lt;");
+            else if (*c == '>')
+                fprintf(html_file, "&gt;");
+            else if (*c == '&')
+                fprintf(html_file, "&amp;");
+            else if (*c == '"')
+                fprintf(html_file, "&quot;");
+            else
+                fprintf(html_file, "%c", *c);
+        }
+    }
+
+    if (in_bold) fprintf(html_file, "</strong>");
+    if (in_italic) fprintf(html_file, "</em>");
+    if (in_code) fprintf(html_file, "</code>");
+}
+
 static inline int convert_file(const char *md_path, const char *html_path) {
     FILE *md_file = NULL, *html_file = NULL;
     char *line = NULL;
@@ -48,7 +99,7 @@ static inline int convert_file(const char *md_path, const char *html_path) {
 
     md_file = fopen(md_path, "r");
     if (!md_file) {
-        fprintf(stderr, "Error: cannot open %s\n", md_path);
+        fprintf(stderr, "Error opening %s: %s\n", md_path, strerror(errno));
         return_defer(1);
     }
 
@@ -94,37 +145,11 @@ static inline int convert_file(const char *md_path, const char *html_path) {
     while (getline(&line, &len, md_file) != -1) {
         line[strcspn(line, "\r\n")] = 0;
 
-        if (strncmp(line, "```", 3) == 0) {
-            if (state != IN_CODE_BLOCK) {
-                if (state == IN_PARAGRAPH) {
-                    fprintf(html_file, "</p>\n");
-                }
-                state = IN_CODE_BLOCK;
-                fprintf(html_file, "<pre><code>");
-            } else {
-                state = OUTSIDE;
-                fprintf(html_file, "</code></pre>\n");
-            }
-            continue;
-        }
-
-        if (state == IN_CODE_BLOCK) {
-            for (char *c = line; *c; c++) {
-                if (*c == '<')
-                    fprintf(html_file, "&lt;");
-                else if (*c == '>')
-                    fprintf(html_file, "&gt;");
-                else if (*c == '&')
-                    fprintf(html_file, "&amp;");
-                else
-                    fprintf(html_file, "%c", *c);
-            }
-            fprintf(html_file, "\n");
-            continue;
-        }
-
         int is_new_block =
-            (*line == '\0' || *line == '#' || (*line == '-' && line[1] == ' '));
+            (*line == '\0' || *line == '#' || (strncmp(line, "```", 3) == 0) ||
+             (strncmp(line, "- ", 2) == 0) ||
+             (isdigit(*line) &&
+              strstr(line, ". ") == line + strspn(line, "0123456789")));
 
         if (state == IN_PARAGRAPH && is_new_block) {
             fprintf(html_file, "</p>\n");
@@ -136,27 +161,52 @@ static inline int convert_file(const char *md_path, const char *html_path) {
             !(line[1] == '.' && line[2] == ' ')) {
             if (state == IN_ORDERED_LIST) fprintf(html_file, "</ol>\n");
             if (state == IN_UNORDERED_LIST) fprintf(html_file, "</ul>\n");
-            state = OUTSIDE;
             list_item_number = 1;
+            state = OUTSIDE;
         }
 
-        if (line[0] == '\0') {
+        if (strncmp(line, "```", 3) == 0) {
+            if (state != IN_CODE_BLOCK) {
+                state = IN_CODE_BLOCK;
+                fprintf(html_file, "<pre><code>");
+            } else {
+                state = OUTSIDE;
+                fprintf(html_file, "</code></pre>\n");
+            }
+            continue;
+        } else if (state == IN_CODE_BLOCK) {
+            for (const char *c = line; *c; c++) {
+                if (*c == '<')
+                    fprintf(html_file, "&lt;");
+                else if (*c == '>')
+                    fprintf(html_file, "&gt;");
+                else if (*c == '&')
+                    fprintf(html_file, "&amp;");
+                else if (*c == '"')
+                    fprintf(html_file, "&quot;");
+                else
+                    fprintf(html_file, "%c", *c);
+            }
+            fprintf(html_file, "\n");
+            continue;
+        } else if (line[0] == '\0') {
             continue;
         } else if (line[0] == '#') {
             int level = 0;
             while (level < 6 && line[level] == '#') level++;
             if (level > 0 && line[level] == ' ') {
-                fprintf(html_file, "<h%d>%s</h%d>\n", level, line + level + 1,
-                        level);
-            } else {
-                fprintf(html_file, "<p>%s</p>\n", line);
+                fprintf(html_file, "<h%d>", level);
+                _inline_formatting(html_file, line + level + 1);
+                fprintf(html_file, "</h%d>\n", level);
             }
         } else if (line[0] == '-' && line[1] == ' ') {
             if (state != IN_UNORDERED_LIST) {
                 state = IN_UNORDERED_LIST;
                 fprintf(html_file, "<ul>\n");
             }
-            fprintf(html_file, "<li>%s</li>\n", line + 2);
+            fprintf(html_file, "<li>");
+            _inline_formatting(html_file, line + 2);
+            fprintf(html_file, "</li>\n");
         } else if (isdigit(line[0])) {
             char *num_end;
             int number = strtol(line, &num_end, 10);
@@ -166,23 +216,20 @@ static inline int convert_file(const char *md_path, const char *html_path) {
                     list_item_number = number;
                     fprintf(html_file, "<ol start=\"%d\">\n", list_item_number);
                 }
-                fprintf(html_file, "<li>%s</li>\n", num_end + 2);
+                fprintf(html_file, "<li>");
+                _inline_formatting(html_file, num_end + 2);
+                fprintf(html_file, "</li>\n");
                 list_item_number++;
-            } else {
-                if (state != IN_PARAGRAPH) {
-                    state = IN_PARAGRAPH;
-                    fprintf(html_file, "<p>%s\n", line);
-                    continue;
-                }
-                fprintf(html_file, "%s\n", line);
             }
         } else {
             if (state != IN_PARAGRAPH) {
                 state = IN_PARAGRAPH;
-                fprintf(html_file, "<p>\n%s\n", line);
+                fprintf(html_file, "<p>\n");
+                _inline_formatting(html_file, line);
+                fprintf(html_file, "\n");
                 continue;
             }
-            fprintf(html_file, "%s\n", line);
+            _inline_formatting(html_file, line);
         }
     }
 
